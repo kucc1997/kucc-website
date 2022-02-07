1946b111f20402c468ea91208679d0dbd34f731f
Amar Mandal <amarmandal2073@gmail.com>
2022-02-07 22:54:57 +0545

    Feat#4: routes configuration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./HOC/Layout";
+import Home from "./Test";
 
 const App = () => {
-  return <Layout />;
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route path="" element={<Home />}></Route>
+				</Route>
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
