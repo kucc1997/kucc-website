cfe868e55ef4bb5a3eb920fec23988d16b18060f
Amar Mandal <amarmandal2073@gmail.com>
2022-02-08 00:28:34 +0545

    feat#4<partial>: add template for out team page

---
+++ src/pages/teams/DocTeam.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DocTeam = () => {
+	return (
+		<div>
+			<h1>Documentation Team Page</h1>
+		</div>
+	);
+};
+
+export default DocTeam;
---
+++ src/pages/teams/ExCommunity.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ExCommunity = () => {
+	return (
+		<div>
+			<h1>Executive Community Page</h1>
+		</div>
+	);
+};
+
+export default ExCommunity;
