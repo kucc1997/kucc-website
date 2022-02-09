83cab03046951abc6928cf8eb67979b32447b58f
Amar Mandal <amarmandal2073@gmail.com>
2022-02-09 20:59:00 +0545

    chore: #4 update .prettierrc

--- src/config/colors.js
+++ src/config/colors.js
@@ -1,12 +1,12 @@
 const COLORS = {
-  primary: "#4ecdc4",
-  secondary: "#fc5c65",
-  black: "#000",
-  white: "#fff",
-  medium: "#6e6969",
-  light: "#f8f4f4",
-  danger: "#ff5252",
-  dark: "#0c0c0c",
-};
+  primary: '#4ecdc4',
+  secondary: '#fc5c65',
+  black: '#000',
+  white: '#fff',
+  medium: '#6e6969',
+  light: '#f8f4f4',
+  danger: '#ff5252',
+  dark: '#0c0c0c',
+}
 
-export default COLORS;
+export default COLORS
