2eeb22146f7d8f3f8cc67396bf165fb61f311cb5
Amar Mandal <amarmandal2073@gmail.com>
2022-02-21 20:58:29 +0545

    chore: updated firebase config file

--- src/pages/events/EventsPage.jsx
+++ src/pages/events/EventsPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { collection, onSnapshot } from '@firebase/firestore'
+import React, { useEffect } from 'react'
+import db from '../../config/firebase'
 
 const EventsPage = () => {
-  return (
-    <div>EventsPage</div>
-  )
+  useEffect(() => {
+    onSnapshot(collection(db, 'events'), (snapshot) => {
+      const eventList = snapshot.docs.map((doc) => doc.data())
+      console.log('All Events ', eventList)
+    })
+  }, [])
+
+  return <div>EventsPage</div>
 }
 
 export default EventsPage
