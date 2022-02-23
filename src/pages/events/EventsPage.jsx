8037f1e0b389e3f8c1d53933f4dcc72b661091b2
Amar Mandal <amarmandal2073@gmail.com>
2022-02-23 12:46:49 +0545

    feat: add style to event page

--- src/pages/events/EventsPage.jsx
+++ src/pages/events/EventsPage.jsx
@@ -2,6 +2,8 @@ import { collection, onSnapshot } from '@firebase/firestore'
 import React, { useEffect } from 'react'
 import db from '../../config/firebase'
 import EventItem from '../../components/Event/EventItem'
+import EventFilter from '../../components/EventFilter/EventFilter'
+import { Container, Box, Typography } from '@mui/material'
 
 const EventsPage = () => {
   useEffect(() => {
@@ -11,7 +13,19 @@ const EventsPage = () => {
     })
   }, [])
 
-  return <EventItem />
+  return (
+    <Container sx={{ pb: 8, pt: 4 }}>
+      <Box>
+        <Typography variant="h4" mb={2}>
+          Events
+        </Typography>
+        <EventFilter />
+      </Box>
+      <Box sx={{ my: 5, display: 'flex', justifyContent: 'center' }}>
+        <EventItem />
+      </Box>
+    </Container>
+  )
 }
 
 export default EventsPage
