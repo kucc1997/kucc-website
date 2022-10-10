39768ed4b1599c3d7d6455065c136902a6caa322
SarinSthapit <sarinsthapit.2001@gmail.com>
2022-10-10 12:47:12 +0545

    Calendar added and few changes made

--- src/components/NewsNotice/NewsNoticeItem.js
+++ src/components/NewsNotice/NewsNoticeItem.js
@@ -1,73 +1,75 @@
 import * as React from 'react'
-import Box from '@mui/material/Box'
+
+import { Box, Container, Grid } from '@mui/material'
+
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
-import COLORS from "../../config/colors";
+import COLORS from '../../config/colors'
 import { color } from '@mui/system'
 
-
 export default function NewsNoticeItem() {
-    return (
-      <Card
-        sx={{ display: 'flex', width: { md: 700, xs: 600 }, height: { md: 136, xs: 300 }, mb: 4 }}
-      >
-        <Card
-            sx={{ display: 'flex', width: { md: 112, xs: 500 }, height: { md: 136, xs: 300 }, mb: 4 }}
-        >
-        <Box alignItems="center" jusifyContent = "center">
-            <Typography component = "div" >
-                <span style={{ fontSize: 70, color: COLORS.medium, fontWeight: "bold"}}>02</span>
-                
-            </Typography>
-            <Typography component = "div" >
-                <span style={{ fontSize: 16, color: COLORS.medium }}>Nov, 2020</span>
-                
-            </Typography>
-        </Box>
-        </Card>
-        
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <CardContent sx={{ flex: '1 0 auto' }}>
-            <Typography component="div" fontSize = "22" fontWeight="bold">
-              KUCC Demonstrated LTSP At Seventh National Science Day
-            </Typography>
-            <Typography
-              color="text.secondary"
-              component="div"
-              fontSize = "12"
-              sx={{ display: 'flex', alignItems: 'flex-start', my: 1 }}
-            >
-              Kathmandu University Computer Club demonstrated Linux Terminal Server Project at Seventh National S.
-            </Typography>
-            <Typography
-              variant="subtitle2"
-              component="div"
-              fontWeight="bold"
-              sx={{ display: 'right', alignItems: 'center' }}
-            >
-             <span style={{ color: COLORS.primary }}>Read more</span> 
-              
-            </Typography>
-          </CardContent>
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              ml: { md: '150px', xs: 3 },
-              py: 2,
-              justifyContent: { md: 'flex-end' },
-            }}
+  return (
+    <Card
+      sx={{ display: 'flex', width: { md: 900, xs: 600 }, height: { md: 136, xs: 300 }, mb: 4 }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={2}>
+        <Box alignItems="center" jusifyContent="center">
+        <Typography component="div">
+          <span style={{ fontSize: 72, color: COLORS.medium, fontWeight: 'bold' }}>02</span>
+        </Typography>
+        <Typography component="div">
+          <span style={{ fontSize: 14, color: COLORS.medium }}>Nov, 2020</span>
+        </Typography>
+      </Box>
+        </Grid>
+        <Grid item xs={8}>
+        <Box sx={{ display: 'center', flexDirection: 'column' }}>
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" fontSize="22" fontWeight="bold" textAlign="left">
+            KUCC Demonstrated LTSP At Seventh National Science Day
+          </Typography>
+          <Typography
+            color="text.secondary"
+            component="div"
+            fontSize="12"
+            textAlign="left"
+            sx={{ display: 'center', alignItems: 'flex-start', my: 1 }}
           >
-            <Button type="submit" variant="contained" color="btn" endIcon={<ArrowForwardIosIcon />}>
-              Explore Event
-            </Button>
-          </Box>
+            Kathmandu University Computer Club demonstrated Linux Terminal Server Project at Seventh
+            National S.
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            component="div"
+            fontWeight="bold"
+            textAlign="left"
+            sx={{ display: 'right', alignItems: 'center' }}
+          >
+            <span style={{ color: COLORS.primary }}>Read more</span>
+          </Typography>
+        </CardContent>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            ml: { md: '150px', xs: 3 },
+            py: 2,
+            justifyContent: { md: 'flex-end' },
+          }}
+        >
+          
         </Box>
-      </Card>
-    )
-  }
-  
+      </Box>
+        </Grid>
+      </Grid>
+      
+
+      
+    </Card>
+  )
+}
--- src/pages/newsAndNotices/NewsNoticePage.jsx
+++ src/pages/newsAndNotices/NewsNoticePage.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Helmet } from 'react-helmet'
 import { Box, Container, Grid, Typography } from '@mui/material'
 import COLORS from '../../config/colors'
 import NewsNoticeList from '../../components/NewsNotice/NewsNoticeList'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
+
 const NewsNoticePage = () => {
+  const [value, onChange] = useState(new Date());
   return (
     <>
       <Helmet>
         <title> News and Notices - KUCC</title>
       </Helmet>
-      
-      <Grid sx={{ background: 'text.light' }} container justify = {"center"} spacing = {0} rowspacing = {1} fluid style={{ paddingLeft: 360, paddingRight: 0 }}>
-        <Grid item xs={6} md={6}>
+
+      <Grid
+        sx={{ background: 'text.light' }}
+        container
+        justify={'center'}
+        spacing={0}
+        rowspacing={1}
+        fluid
+        style={{ paddingLeft: 60, paddingRight: 0 }}
+      >
+        <Grid item xs={6} md={8}>
           <Container sx={{ py: 4 }}>
             <Box textAlign="center" mb={1}>
               <Typography variant="subtitle1" color="text.secondary">
@@ -28,34 +40,79 @@ const NewsNoticePage = () => {
         </Grid>
         <Grid item xs={6} md={4}>
           <Container sx={{ py: 6 }}>
-            <Box textAlign="center" mb={3}>
+            <Box mb={3}>
               <Typography>
-                <span style={{ fontWeight: 'bold', fontSize: '34' }}>Monthly Archives {'\n'}</span>
+                <span style={{ fontWeight: 'bold', fontSize: 24 }}>Monthly Archives {'\n'}</span>
               </Typography>
               <br></br>
               <Typography>
-                <span style={{ fontSize: '4', textAlign: 'right' }}>Archives<br/></span>
+                <span
+                  style={{
+                    fontSize: 14,
+                    textAlign: 'left',
+                    color: COLORS.dark,
+                    fontWeight: 'bold',
+                  }}
+                >
+                  Archives
+                </span>
               </Typography>
               <br></br>
-              <Typography><div style={{ display: 'inline', float: 'center' }}>
-              <ol style={{ listStyleType: 'none', color:COLORS.primary }}>
-                <li>September 2022 (5)</li>
-                <li>August 2022 (5)</li>
-                <li>July 2022 (5)</li>
-                <li>June 2022 (5)</li>
-                <li>May 2022 (5)</li>
-                <li>April 2022 (5)</li>
-                <li>March 2022 (5)</li>
-                <li>February 2022 (5)</li>
-                <li>January 2022 (5)</li>
-                <li>December 2021 (5)</li>
-                <li>November 2021 (5)</li>
-                <li>October 2021 (5)</li>
-              </ol>
-              </div></Typography>
-              <br></br><br></br><br></br><br></br>
+              <Typography>
+                <div style={{ display: 'inline', float: 'center' }}>
+                  <Box ml={3}>
+                    <ol style={{ listStyleType: 'disc', color: COLORS.dark }}>
+                      <li>
+                        <span style={{ color: COLORS.primary }}>September 2022</span> (5)
+                      </li>
+                      <li>
+                        <span style={{ color: COLORS.primary }}>August 2022</span> (5)
+                      </li>
+                      <li>
+                        <span style={{ color: COLORS.primary }}>July 2022 </span>(5)
+                      </li>
+                      <li>
+                        <span style={{ color: COLORS.primary }}>June 2022 </span>(5)
+                      </li>
+                      <li>
+                        <span style={{ color: COLORS.primary }}>May 2022 </span>(5)
+                      </li>
+                      <li>
+                        <span style={{ color: COLORS.primary }}>April 2022 </span>(5)
+                      </li>
+                      <li>
+                        <span style={{ color: COLORS.primary }}>March 2022 </span>(5)
+                      </li>
+                      <li>
+                        <span style={{ color: COLORS.primary }}>February 2022 </span>(5)
+                      </li>
+                      <li>
+                        <span style={{ color: COLORS.primary }}>January 2022 </span>(5)
+                      </li>
+                      <li>
+                        <span style={{ color: COLORS.primary }}>December 2021 </span>(5)
+                      </li>
+                      <li>
+                        <span style={{ color: COLORS.primary }}>November 2021 </span>(5)
+                      </li>
+                      <li>
+                        <span style={{ color: COLORS.primary }}>October 2021 </span>(5)
+                      </li>
+                    </ol>
+                  </Box>
+                </div>
+              </Typography>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
               <Typography>
                 <span style={{ fontWeight: 'bold', fontSize: '28' }}>Calendar</span>
+                <br></br>
+                <div>
+                  
+                  <Calendar onChange={onChange} value={value} />
+                </div>
               </Typography>
             </Box>
           </Container>
