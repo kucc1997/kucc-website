63f13f73728487863986b13fd8ae16dafdfd92e2
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-03-31 22:54:25 +0545

    add transitions for sidebars

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -44,10 +44,11 @@ const NavBar = () => {
         </Link>
       </div>
 
-      {/* hambuger menu */}
+      {/* mobile view */}
       <div className="flex items-center lg:hidden" ref={sidebarRef}>
+        {/* hambuger menu */}
         <button
-          className="flex z-50 justify-center relative cursor-pointer items-center flex-col space-y-1 aspect-square"
+          className="flex z-50 justify-center relative cursor-pointer items-center p-2 flex-col space-y-1 aspect-square hover:bg-[#0002] rounded transition-all"
           onClick={() => setOpen(!open)}
         >
           <div
@@ -67,104 +68,103 @@ const NavBar = () => {
           ></div>
         </button>
 
-        {/* mobile view */}
-        {open && (
-          <div className="flex space-y-8 z-40 h-screen bg-white fixed top-0 right-0 w-2/3 sm:w-1/3 md:w-2/4 shadow-xl">
-            <div className="flex-col mt-28 px-8 space-y-8 font-sans list-none">
-              <li className="text-teal-600 font-semibold transition cursor-pointer border-b-2 items-center">
-                <Link to="/" className="flex gap-4">
-                  <Home />
-                  HOME
-                </Link>
-              </li>
-              {/* our team dropdown */}
-              <li>
-                <div className="text-teal-600 font-semibold transition-all cursor-pointer relative md:group border-b-2 flex gap-4">
-                  <Users />
-                  OUR TEAM{' '}
-                  <button className="flex rounded-full" onClick={() => setOurTeam(!ourTeam)}>
-                    {!ourTeam ? (
-                      <ChevronDown className="bg-teal-100 rounded-md" />
-                    ) : (
-                      <ChevronUp className="bg-teal-100 rounded-md" />
-                    )}
-                  </button>
-                </div>
-                {ourTeam && (
-                  <ul className="w-full space-y-2 z-10 p-[20px] list-none">
-                    <li className="cursor-pointer border-b border-gray-500 text-base hover:text-teal-800">
-                      <Link to="executive-committee">EXECUTIVE COMMITTEE</Link>
-                    </li>
-                    <li className="cursor-pointer border-b border-gray-500 text-base hover:text-teal-800">
-                      <Link to="advisory-board">ADVISORY BOARD</Link>
-                    </li>
-                    <li className="cursor-pointer border-b border-gray-500 text-base hover:text-teal-800">
-                      <Link to="community-coordinators">COMMUNITY COORDINATORS</Link>
-                    </li>
-                    <li className="cursor-pointer border-b border-gray-500 text-base hover:text-teal-800">
-                      <Link to="documentation-teams">DOCUMENTATION TEAM</Link>
-                    </li>
-                  </ul>
-                )}
-              </li>
-
-              {/* publications dropdown */}
-              <li>
-                <div className="text-teal-600 font-semibold transition cursor-pointer relative md:group border-b-2 flex gap-4">
-                  <NotebookPen />
-                  PUBLICATIONS{' '}
-                  <button className="flex rounded-full" onClick={() => setOurPub(!ourPub)}>
-                    {!ourPub ? (
-                      <ChevronDown className="bg-teal-100 rounded-md" />
-                    ) : (
-                      <ChevronUp className="bg-teal-100 rounded-md" />
-                    )}
-                  </button>
-                </div>
-                <ul
-                  className={`space-y-2 z-10 p-[20px] origin-top list-none ${ourPub ? 'scale-y-100 static' : 'scale-y-0 absolute'}`}
-                >
+        <div
+          className={`flex space-y-8 transition origin-right z-40 h-screen bg-white fixed top-0 right-0 w-2/3 sm:w-1/3 md:w-2/4 shadow-xl ${open ? 'scale-x-100' : 'scale-x-0'}`}
+        >
+          <div className="flex-col mt-28 px-8 space-y-8 font-sans list-none">
+            <li className="text-teal-600 font-semibold transition cursor-pointer border-b-2 items-center">
+              <Link to="/" className="flex gap-4">
+                <Home />
+                HOME
+              </Link>
+            </li>
+            {/* our team dropdown */}
+            <li>
+              <div className="text-teal-600 font-semibold transition-all cursor-pointer relative md:group border-b-2 flex gap-4">
+                <Users />
+                OUR TEAM{' '}
+                <button className="flex rounded-full" onClick={() => setOurTeam(!ourTeam)}>
+                  {!ourTeam ? (
+                    <ChevronDown className="bg-teal-100 rounded-md" />
+                  ) : (
+                    <ChevronUp className="bg-teal-100 rounded-md" />
+                  )}
+                </button>
+              </div>
+              {ourTeam && (
+                <ul className="w-full space-y-2 z-10 p-[20px] list-none">
                   <li className="cursor-pointer border-b border-gray-500 text-base hover:text-teal-800">
-                    <Link to="it-express">IT EXPRESS</Link>
+                    <Link to="executive-committee">EXECUTIVE COMMITTEE</Link>
                   </li>
                   <li className="cursor-pointer border-b border-gray-500 text-base hover:text-teal-800">
-                    <Link to="blogs">STUDENT BLOGS</Link>
+                    <Link to="advisory-board">ADVISORY BOARD</Link>
+                  </li>
+                  <li className="cursor-pointer border-b border-gray-500 text-base hover:text-teal-800">
+                    <Link to="community-coordinators">COMMUNITY COORDINATORS</Link>
+                  </li>
+                  <li className="cursor-pointer border-b border-gray-500 text-base hover:text-teal-800">
+                    <Link to="documentation-teams">DOCUMENTATION TEAM</Link>
                   </li>
                 </ul>
-              </li>
-              <li className="text-teal-600 font-semibold border-b-2">
-                <Link to="news-and-notices" className="flex gap-4">
-                  <Newspaper />
-                  NEWS AND NOTICES
-                </Link>
-              </li>
-              <li className="text-teal-600 font-semibold border-b-2">
-                <Link to="events" className="flex gap-4">
-                  <CalendarSearch />
-                  EVENTS
-                </Link>
-              </li>
-              <li className="text-teal-600 font-semibold border-b-2">
-                <Link to="code-of-conduct" className="flex gap-4">
-                  <BookKey />
-                  CODE OF CONDUCT
-                </Link>
-              </li>
-              <li className="text-teal-600 font-semibold border-b-2">
-                <Link to="contact" className="flex gap-4">
-                  <Contact />
-                  CONTACT
-                </Link>
-              </li>
-              <li className="text-teal-600 font-semibold border-b-2">
-                <Link to="register" className="flex gap-4">
-                  <BadgeCheck />
-                  APPLY TO BE A MEMBER
-                </Link>
-              </li>
-            </div>
+              )}
+            </li>
+
+            {/* publications dropdown */}
+            <li>
+              <div className="text-teal-600 font-semibold transition cursor-pointer relative md:group border-b-2 flex gap-4">
+                <NotebookPen />
+                PUBLICATIONS{' '}
+                <button className="flex rounded-full" onClick={() => setOurPub(!ourPub)}>
+                  {!ourPub ? (
+                    <ChevronDown className="bg-teal-100 rounded-md" />
+                  ) : (
+                    <ChevronUp className="bg-teal-100 rounded-md" />
+                  )}
+                </button>
+              </div>
+              <ul
+                className={`space-y-2 z-10 p-[20px] origin-top list-none ${ourPub ? 'scale-y-100 static' : 'scale-y-0 absolute'}`}
+              >
+                <li className="cursor-pointer border-b border-gray-500 text-base hover:text-teal-800">
+                  <Link to="it-express">IT EXPRESS</Link>
+                </li>
+                <li className="cursor-pointer border-b border-gray-500 text-base hover:text-teal-800">
+                  <Link to="blogs">STUDENT BLOGS</Link>
+                </li>
+              </ul>
+            </li>
+            <li className="text-teal-600 font-semibold border-b-2">
+              <Link to="news-and-notices" className="flex gap-4">
+                <Newspaper />
+                NEWS AND NOTICES
+              </Link>
+            </li>
+            <li className="text-teal-600 font-semibold border-b-2">
+              <Link to="events" className="flex gap-4">
+                <CalendarSearch />
+                EVENTS
+              </Link>
+            </li>
+            <li className="text-teal-600 font-semibold border-b-2">
+              <Link to="code-of-conduct" className="flex gap-4">
+                <BookKey />
+                CODE OF CONDUCT
+              </Link>
+            </li>
+            <li className="text-teal-600 font-semibold border-b-2">
+              <Link to="contact" className="flex gap-4">
+                <Contact />
+                CONTACT
+              </Link>
+            </li>
+            <li className="text-teal-600 font-semibold border-b-2">
+              <Link to="register" className="flex gap-4">
+                <BadgeCheck />
+                APPLY TO BE A MEMBER
+              </Link>
+            </li>
           </div>
-        )}
+        </div>
       </div>
 
       {/* desktop view */}
