9838826f52de698e3b57bbbe46f50c4846e92de7
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-04-05 06:18:22 +0545

    add title to every page

--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -7,18 +7,25 @@ import CommunitiesActive from './components/CommunitiesActive'
 import Activities from './components/Activities'
 import CounterUp from './components/CounterUp'
 import PresidentSection from './components/PresidentSection'
-import Articles from './components/Articles'
+// import Articles from './components/Articles'
+import { Helmet } from 'react-helmet'
+
 export default function HomePage() {
-  return (
-    <div className="flex-col space-y-10 w-100vw">
-      {/* <Slider /> */}
-      <KuccDescription />
-      <KUOSC />
-      <CommunitiesActive />
-      <Activities />
-      <CounterUp />
-      <PresidentSection />
-      {/* <Articles /> */}
-    </div>
-  )
+	return (
+		<>
+			<Helmet>
+				<title>Kathmandu University Computer Club (KUCC)</title>
+			</Helmet>
+			<div className="flex-col space-y-10 w-100vw">
+				{/* <Slider /> */}
+				<KuccDescription />
+				<KUOSC />
+				<CommunitiesActive />
+				<Activities />
+				<CounterUp />
+				<PresidentSection />
+				{/* <Articles /> */}
+			</div>
+		</>
+	)
 }
