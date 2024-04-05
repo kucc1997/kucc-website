fae45ca805a185cac6ae899bcb279790238dc2a5
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-04-05 06:09:22 +0545

    add page not found page

--- src/App.js
+++ src/App.js
@@ -3,50 +3,49 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material'
 import theme from './theme'
 import Layout from './HOC/Layout'
-import Home from './Test'
 import ExCommunityPage from './pages/teams/ExCommunity'
 import AdvBoardPage from './pages/teams/AdvBoard'
 import DocTeamPage from './pages/teams/DocTeam'
 import CommunityCoPage from './pages/teams/CommunityCo'
 import CodeOfConductPage from './pages/CodeOfConductPage'
 import ContactPage from './pages/ContactPage'
-// import RegisterPage from './pages/member/RegisterPage'
 import './input.css'
 import NewsNoticePage from './pages/newsAndNotices/NewsNoticePage'
 import EventsPage from './pages/events/EventsPage'
 import ItExpressPage from './pages/publications/ItExpressPage'
 
-import BlogsPage from './pages/publications/ItExpressPage'
 import StudentBlog from './components/StudentBlog'
 import BlogPage from './components/BlogPage'
 
 import HomePage from './pages/homePage/homePage'
+import NotFound from './pages/NotFound'
 
 const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="" element={<HomePage />} />
-            <Route path="advisory-board" element={<AdvBoardPage />} />
-            <Route path="executive-committee" element={<ExCommunityPage />} />
-            <Route path="community-coordinators" element={<CommunityCoPage />} />
-            <Route path="documentation-teams" element={<DocTeamPage />} />
-            <Route path="code-of-conduct" element={<CodeOfConductPage />} />
-            <Route path="contact" element={<ContactPage />} />
-            {/* <Route path="register" element={<RegisterPage />} /> */}
-            <Route path="events" element={<EventsPage />} />
-            <Route path="it-express" element={<ItExpressPage />} />
+	return (
+		<ThemeProvider theme={theme}>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Layout />}>
+						<Route path="" element={<HomePage />} />
+						<Route path="advisory-board" element={<AdvBoardPage />} />
+						<Route path="executive-committee" element={<ExCommunityPage />} />
+						<Route path="community-coordinators" element={<CommunityCoPage />} />
+						<Route path="documentation-teams" element={<DocTeamPage />} />
+						<Route path="code-of-conduct" element={<CodeOfConductPage />} />
+						<Route path="contact" element={<ContactPage />} />
+						{/* <Route path="register" element={<RegisterPage />} /> */}
+						<Route path="events" element={<EventsPage />} />
+						<Route path="it-express" element={<ItExpressPage />} />
 
-            <Route path="news-and-notices" element={<NewsNoticePage />} />
-            <Route path="blogs" element={<StudentBlog />} />
-            <Route path="blog/:id" element={<BlogPage />} />
-          </Route>
-        </Routes>
-      </Router>
-    </ThemeProvider>
-  )
+						<Route path="news-and-notices" element={<NewsNoticePage />} />
+						<Route path="blogs" element={<StudentBlog />} />
+						<Route path="blog/:id" element={<BlogPage />} />
+						<Route path="*" element={<NotFound />} />
+					</Route>
+				</Routes>
+			</Router>
+		</ThemeProvider>
+	)
 }
 
 export default App
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+
+export default function NotFound() {
+	return (
+		<div className="flex-col space-y-8 p-6 h-full">
+			<div className="flex-col">
+				<h1 className="text-[#00baa3] text-lg sm:text-2xl md:text-3xl font-bold text-center py-5">
+					Error 404: Page Not Found
+				</h1>
+				<div className="flex-col items-center justify-center">
+					<h1 className="text-[#00baa3] text-lg sm:text-2xl md:text-3xl font-bold text-center">
+						Oops! The page you are looking for does not exist.
+					</h1>
+					<Link
+						to="/"
+						className="text-lg font-bold text-center text-blue-500 hover:underline underline-offset-1 mx-auto block py-2"
+					>
+						Go back to the home page
+					</Link>
+				</div>
+			</div>
+		</div>
+	)
+}
