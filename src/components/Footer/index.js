b919dda9071160f289095455b0673f17713d08b8
irhs0 <119833580+shri-acha@users.noreply.github.com>
2024-04-25 14:55:38 +0000

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -75,21 +75,15 @@ export default function Footer() {
                     return <p key={index}>{link.title}</p>
                   }
 
-   return ( 
-     
+ return (           
 	 link.link.startsWith("https://")
 	 	?<a href={link.link} target={link.target ? link.target : '_self'} key={index}>
   				<p>{link.title}</p>
 	 	</a>
-		:<Link to={link.link} target={link.target ? link.target : '_self'} key={index}>		            
-      <p>{link.title}</p> 
+		:<Link to={link.link} target={link.target ? link.target : '_self'} key={index}>
+	 		<p>{link.title}</p> 
 	 	</Link>
-  
-)	
-                })}
-              </div>
-            )
-
+  )	
                 })}
               </div>
             )
