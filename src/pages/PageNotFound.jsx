75abb32788ee0ee7d5c10d4736b539f7b83f360a
Razzat Dahal <rajat4623@student.ku.edu.np>
2024-04-26 11:31:44 +0545

    added: link to the homepage when 404

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
@@ -10,7 +11,8 @@ const PageNotFound = () => {
       />
       <div className="text-center">
         <h1 className="text-4xl font-bold text-gray-900 mb-2">404 Page Not Found</h1>
-        <p className="text-lg text-gray-600">The page you're looking for doesn't exist.</p>
+        <p className="text-lg text-gray-600">
+        Go back to <Link to="/" className="text-teal-600" >home page</Link>. </p>
       </div>
     </div>
   );
