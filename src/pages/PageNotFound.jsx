b2dce0f7770ab9de586a86dc346b6311f556e60a
asdfjkl <rajat4623@student.ku.edu.np>
2024-04-28 13:33:16 +0000

    fix of issue no #71 (#74)
    * added: routing for invalid page links
    
    fix for (#71)
    
    * added: png image for error 404
    
    * added: link to the homepage when 404

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const PageNotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <img
+        src="/assets/images/404-image.png" // Replace with the path to your 404 image
+        alt="Could not load image"
+        className="w-64 h-auto mb-4"
+      />
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gray-900 mb-2">404 Page Not Found</h1>
+        <p className="text-lg text-gray-600">
+        Go back to <Link to="/" className="text-teal-600" >home page</Link>. </p>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
