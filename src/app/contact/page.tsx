5b940a7c466061dccac60d6a911853f6b7e517e1
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-09-03 19:20:39 +0545

    add location pointer in contact map iframe

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -24,13 +24,13 @@ const ContactPage = () => {
           </Grid>
           <Grid item xs={12} mt={4}>
             <iframe
-              title="ku-location"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3535.1602564910536!2d85.53645301542707!3d27.61955293595072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb092082d99e3b%3A0xe509dafa47685dba!2sKathmandu%20University!5e0!3m2!1sen!2sus!4v1644506871909!5m2!1sen!2sus"
+              src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=ku-location&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
               width="100%"
               height="450"
               style={{ border: 0 }}
               allowFullScreen
               loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
           </Grid>
         </Grid>
