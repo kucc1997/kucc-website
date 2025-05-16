45df76ab5a16d2bcf5834f8dd16d9488672f249d
Bishist Bikram Pant <bishist.b.pant@gmail.com>
2025-05-16 21:56:56 +0545

    Added Sanjog Sir's profile in Advisory Board, Updated from KUCC board 2023-2024 to KUCC board 2025

--- src/app/advisory-board/page.tsx
+++ src/app/advisory-board/page.tsx
@@ -19,11 +19,18 @@ export const metadata: Metadata = {
 const advisorsList = [
   {
     name: 'Asst. Prof. Pankaj Raj Dawadi, PhD',
-    position: 'Acting, Head of Department, DoCSE',
+    position: 'Acting Head of Department, DoCSE',
     image: '/people/Pankaj_Raj_Dawadi.webp',
     linkedin: 'https://www.linkedin.com/in/pankaj-dawadi-83467b7a/',
     mail: 'pdawadi@ku.edu.np',
   },
+  {
+    name: 'Sanjog Sigdel',
+    position: 'Lecturer, DoCSE',
+    inage: '/people/Sanjog_Sigdel.jpg',
+    linkedin: 'https://www.linkedin.com/in/sigdelsanjog ',
+    mail: 'sanjog.sigdel@ku.edu.np'
+  }
 ]
 
 export default function AdvBoard() {
@@ -31,7 +38,7 @@ export default function AdvBoard() {
     <div className='flex flex-col p-5 justify-center items-center max-w-screen-xl mx-auto'>
       <Card className='w-full border-2 shadow-lg bg-baseBackground px-4'>
         <CardHeader className='flex items-center'>
-          <CardDescription className='text-gray-500 text-lg'>KUCC Board 2023-24</CardDescription>
+          <CardDescription className='text-gray-500 text-lg'>KUCC Board 2025</CardDescription>
           <CardTitle className='text-titleColor text-3xl'>Advisors</CardTitle>
         </CardHeader>
         <CardContent className="flex flex-col space-y-5 lg:hidden justify-center items-center rounded-xl">
